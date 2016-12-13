aa7f0bf746a235705dfaf140ed8e4ac54cdc1c7d
manoj@conqsys.com <manoj@conqsys.com>
2016-12-13 18:08:15 +0530

    resolve conflicts on invoice-detail.component and resolve dashboard issues

--- src/app/invoice/shared/invoice.service.ts
+++ src/app/invoice/shared/invoice.service.ts
@@ -10,100 +10,108 @@ export class InvoiceService {
   constructor(private http: Http) {
   }
 
-
-  getInvoices(searchFields) {
-    searchFields = {
-      companyID: 0,
-      currentPage: 1,
-      invFromDate: "",
-      invToDate: "",
-      invoiceDesc: "",
-      invoiceNumber: "",
-      pageSize: 25,
-      statusID: -1,
-      userID: -1,
-      vendorID: 0
-    }
-
-    var data = JSON.stringify(searchFields);
+  public getInvoices(searchFields) {
+    searchFields.statusId = searchFields.statusId === 0 ? -1 : searchFields.statusId;
+    searchFields.userId = searchFields.userId === 0 ? -1 : searchFields.userId;
+    let data = JSON.stringify(searchFields);
     return this.http
-      .post(ApiUrl.baseUrl + "api/invoices/PostSearchTogetInvoices", data)
+      .post(ApiUrl.baseUrl + 'api/invoices/PostSearchTogetInvoices', data)
       .toPromise()
       .then(response => response.json())
       .catch(error => error);
   }
 
-  getInvoicesCheckDetailsByInvoiceNumber(invSearchData) {
-    var data = JSON.stringify(invSearchData);
-    return this.http.post(ApiUrl.baseUrl + "api/invoices/PostInvoicesCheckDetailsByInvoiceNumber/", data)
+  public getInvoicesCheckDetailsByInvoiceNumber(invSearchData) {
+    let data = JSON.stringify(invSearchData);
+    return this.http.post(ApiUrl.baseUrl + 'api/invoices/PostInvoicesCheckDetailsByInvoiceNumber/', data)
       .toPromise()
       .then(response => response.json())
       .catch(error => error);
   }
 
-  getInvoiceId(invSearchObject) {
-    var data = JSON.stringify(invSearchObject);
+  public getInvoiceId(invSearchObject) {
+    let data = JSON.stringify(invSearchObject);
     return this.http
-      .post(ApiUrl.baseUrl + "api/invoices/PostSearchTogetInvoicesIds/", data)
+      .post(ApiUrl.baseUrl + 'api/invoices/PostSearchTogetInvoicesIds/', data)
       .toPromise()
       .then(response => response.json() as InvoiceModel)
       .catch(error => error);
   }
 
-  getInvoiceDetail(invoiceId, attachmentId, isFromEditWindow) {
+  public getInvoiceDetail(invoiceId, attachmentId, isFromEditWindow) {
     return this.http
-      .get(ApiUrl.baseUrl + "api/invoices/" + invoiceId + "/" + attachmentId + "/" + isFromEditWindow)
+      .get(ApiUrl.baseUrl + 'api/invoices/' + invoiceId + '/' + attachmentId + '/' + isFromEditWindow)
       .toPromise()
       .then(response => response.json())
       .catch(error => error);
   }
 
-  getInvoiceApprovals(invoiceId, InvoiceAmount, CompanyId) {
+  public getInvoiceApprovals(invoiceId, InvoiceAmount, CompanyId) {
 
     let urlString = ApiUrl.baseUrl
-      + "api/invoices/approvals/"
-      + invoiceId + "/"
-      + InvoiceAmount + "/"
+      + 'api/invoices/approvals/'
+      + invoiceId + '/'
+      + InvoiceAmount + '/'
       + CompanyId;
 
     return this.http.get(urlString)
       .toPromise()
       .then(response => response.json())
       .catch(error => error);
-    
+
   }
-  checkInvoiceNumberExists(param) {
-    var data = JSON.stringify(param);
-    return this.http.post(ApiUrl.baseUrl + "api/invoices/PostSearchToInvoiceNumberExists/", data)
+
+  public submitInvoiceExpedite(invoiceID) {
+    return this.http.get(ApiUrl.baseUrl
+      + 'api/invoices/getInvoiceExpedite/' + invoiceID)
       .toPromise()
       .then(response => response.json())
       .catch(error => error);
-    
   }
-   removeInvoiceDistributions(distID, invoiceID) {
+
+
+  public submitInvoiceForApproval(invoiceID) {
+    return this.http.get(ApiUrl.baseUrl + 'api/invoices/InvoiceForApproval/' + invoiceID)
+      .toPromise()
+      .then(response => response.json())
+      .catch(error => error);
+  }
+
+  public checkInvoiceNumberExists(param) {
+    let data = JSON.stringify(param);
+    return this.http.post(ApiUrl.baseUrl + 'api/invoices/PostSearchToInvoiceNumberExists/', data)
+      .toPromise()
+      .then(response => response.json())
+      .catch(error => error);
+
+  }
+
+  public removeInvoiceDistributions(distID, invoiceID) {
     return this.http
-      .get(ApiUrl.baseUrl + "api/invoices/removeDist/" + distID + "/" + invoiceID)
+      .get(ApiUrl.baseUrl + 'api/invoices/removeDist/' + distID + '/' + invoiceID)
       .toPromise()
       .then(response => response.json())
       .catch(error => error);
   }
- getNextAttachmentIDs(attachmentID:number): Promise<any> {
-    
+
+  public getNextAttachmentIDs(attachmentID: number): Promise<any> {
+
     return this
       .http
-      .get(ApiUrl.baseUrl + 'api/invoices/nextAttachments/'+ attachmentID)
+      .get(ApiUrl.baseUrl + 'api/invoices/nextAttachments/' + attachmentID)
       .toPromise()
       .then(response => response.json())
       .catch(this.handleError);
   }
-   saveInvoice(invoiceDetail){
-      var data = JSON.stringify(invoiceDetail);
+
+  public saveInvoice(invoiceDetail) {
+    let data = JSON.stringify(invoiceDetail);
     return this
       .http
-      .post(ApiUrl.baseUrl + "api/invoices/newOrUpdate", data)
+      .post(ApiUrl.baseUrl + 'api/invoices/newOrUpdate', data)
       .toPromise()
       .then(response => response.json())
-        .catch(error => error);
+      .catch(error => error);
   }
 
   public handleError(error: any): Promise<any> {
